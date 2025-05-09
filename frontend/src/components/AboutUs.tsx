2bfef2ae157537fed3b0b075b087204fb02b6674
trungle15 <50526463+trungle15@users.noreply.github.com>
2025-05-08 20:52:00 -0500

    add new image for about

--- frontend/src/components/AboutUs.tsx
+++ frontend/src/components/AboutUs.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 import Image from "next/image";
 
 type AboutUsProps = {
@@ -8,34 +8,37 @@ type AboutUsProps = {
   ourTeam: string;
 };
 
-export default function AboutUs({ abtUsTitle, description, ourTeam }: AboutUsProps) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-cyan-600 px-4 text-center">
-
-        {/* Optional mascot image */}
-        <Image
-        src="/images/trashcan_test_image.jpg"
+export default function AboutUs({
+  abtUsTitle,
+  description,
+  ourTeam,
+}: AboutUsProps) {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-cyan-600 px-4 text-center">
+      {/* Optional mascot image */}
+      <Image
+        src="/images/Trash-chan .png"
         alt="Cartoon Can Mascot"
         width={180}
         height={180}
         className="mascot rounded-lg border-2 border-lime-500 mb-4"
-        />
-  
-        {/* The description for the about us page */}
-        <div className="bg-white rounded-lg shadow-lg p-8 max-w-3xl border-t-4 border-lime-500">
-          <h1 className="text-2xl font-bold text-lime-800 mb-4">{abtUsTitle}</h1>
-          <p className="text-cyan-950 leading-relaxed">
-            {description}
-          </p>
-          <h2 className="text-xl font-semibold text-lime-800 mt-6 mb-2">{ourTeam}</h2>
-        </div>
-        {/* Back button in top-left corner */}
-        <Link
-                href="/"
-                className="bg-lime-500 text-lime-800 px-4 py-2 rounded hover:bg-lime-600 flex items-center gap-2 mt-4"
-            >
-                ← Back
-            </Link>
+      />
+
+      {/* The description for the about us page */}
+      <div className="bg-white rounded-lg shadow-lg p-8 max-w-3xl border-t-4 border-lime-500">
+        <h1 className="text-2xl font-bold text-lime-800 mb-4">{abtUsTitle}</h1>
+        <p className="text-cyan-950 leading-relaxed">{description}</p>
+        <h2 className="text-xl font-semibold text-lime-800 mt-6 mb-2">
+          {ourTeam}
+        </h2>
       </div>
-    );
-  }
+      {/* Back button in top-left corner */}
+      <Link
+        href="/"
+        className="bg-lime-500 text-lime-800 px-4 py-2 rounded hover:bg-lime-600 flex items-center gap-2 mt-4"
+      >
+        ← Back
+      </Link>
+    </div>
+  );
+}
